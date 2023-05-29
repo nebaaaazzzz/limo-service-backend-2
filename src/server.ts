0cff8cab5ddaf1f582fae7c55d6895e13e5c57c7
Nebiyu <60134585+nebaaaazzzz@users.noreply.github.com>
2023-05-29 17:14:37 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,15 @@ const PORT = process.env.PORT || 3030;
 (async () => {
   try {
     await User.create({
-   
-        email: "neba@gmail.com",
+   data:{
+     email: "neba@gmail.com",
         lastName: "Daniel",
         firstName: "Nebiyu",
         // password: "123456",
         password:
           "$2a$10$Bl4Y5US/Gh2bOn6GchivH.GUjKcbEp.h9q8gHaEenpoJ1GMDgLlmi",
+   }
+        
       
     });
   } catch (e: any) {
